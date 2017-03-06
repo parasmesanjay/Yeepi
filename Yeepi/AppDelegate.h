d675fea10f1ca96b51d9d3f489baf00628239751
Apple <Apple@apples-macbook.local>
2017-03-06 10:47:41 +0530

    Initial Commit

---
+++ Yeepi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Yeepi
+//
+//  Created by Apple on 06/03/17.
+//  Copyright © 2017 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Yeepi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Yeepi
+//
+//  Created by Apple on 06/03/17.
+//  Copyright © 2017 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
